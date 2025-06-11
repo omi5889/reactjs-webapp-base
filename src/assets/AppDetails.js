d687f0ceea7ae80fe00fd0839299586fe2a0f214
Omkar Gadgil <omi5889@gmail.com>
2025-06-11 19:30:40 -0400

    adding new app - "Decider"

--- src/assets/AppDetails.js
+++ src/assets/AppDetails.js
@@ -13,6 +13,13 @@ const apps = [
       "This application fetches the Astronomy Picture of the day using NASA's APOD API. It displays the image along with its title and explanation.",
     url: "https://pines-rjs-apod.netlify.app/",
   },
+  {
+    id: 3,
+    name: "Random Decision App",
+    description:
+      "This application allows users to make random decisions by providing options. It randomly selects one of the provided options.",
+    url: "https://pines-rjs-decider.netlify.app/",
+  },
 ];
 
 export default apps;
